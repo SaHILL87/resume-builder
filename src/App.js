9c5b9341a59768deaa795b6bb239600cc19e53e5
Sahil Gurnani <gurnanisahil87@gmail.com>
2024-04-17 21:43:38 +0530

    initial 01

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
-
 import "./App.css";
+import Login from "./components/Login/login";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Body />
-    </div>
+    <Router>
+      <div>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/build-resume" element={<Body />} />
+          <Route path="/" element={<Header />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,41 @@
 import React from "react";
-
-import resumeSvg from "../../assets/resume.svg";
-
+import resumeSvg from "../../assets/makemyresumelogo.png";
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+></link>;
 
 function Header() {
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <p className={styles.heading}>
-          A <span>Resume</span> that stands out!
-        </p>
-        <p className={styles.heading}>
-          Make your own resume. <span>It's free</span>
-        </p>
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          <img src={resumeSvg} alt="Company Logo" />
+          <span className={styles.companyName}>MakeMyResume</span>
+        </div>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/build-resume">Build Resume</Link>
+          <Link to="/contact-us">Contact Us</Link>
+        </ul>
       </div>
-      <div className={styles.right}>
-        <img src={resumeSvg} alt="Resume" />
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <p className={styles.heading}>
+            A <span>Resume</span> that stands out!
+          </p>
+          <p className={styles.heading}>
+            Make your own resume. <span>It's free</span>
+          </p>
+        </div>
+        <div className={styles.right}>
+          <img src={resumeSvg} alt="Resume" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
